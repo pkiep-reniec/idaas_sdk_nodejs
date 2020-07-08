bbcbb5e10e1082d744031eaaff0f3e50649ebc08
Miguel Pazo <miguel.ps19@gmail.com>
2020-07-07 23:09:06 -0500

    update readme

--- constants/constAuth.js
+++ constants/constAuth.js
@@ -3,22 +3,15 @@
  */
 
 module.exports = Object.freeze({
-    ACR_ONE_FACTOR: 'one_factor',
-    ACR_TWO_FACTOR: 'two_factor',
     ACR_PKI_DNIE: 'pki_dnie',
     ACR_PKI_TOKEN: 'pki_token',
-    ACR_ONLY_PASSWORD: 'only_password',
-    ACR_ONLY_QUESTIONS: 'only_questions',
     PKI_DNIE_MOBILE: 'pki_dnie_mobile',
-    FINGERPRINT_MOBILE: 'fingerprint_mobile',
     PKI_DNIE_LEGACY: 'pki_dnie_legacy',
     PKI_TOKEN_LEGACY: 'pki_token_legacy',
     PROMPT_NONE: 'none',
     PROMPT_LOGIN: 'login',
     PROMPT_CONSENT: 'consent',
     SCOPE_PROFILE: 'profile',
-    SCOPE_EMAIL: 'email',
-    SCOPE_PHONE: 'phone',
     SCOPE_OFFLINE_ACCESS: 'offline_access',
     RESPONSE_TYPE_CODE: 'code',
     RESPONSE_TYPE_ID_TOKEN: 'id_token',
